767f1f70bb0c8ed43e6fd39abc1800f7e6a7eba8
Ashish Agarwal <Ashish.Agarwal@adp.com>
2015-05-03 11:34:39 +0530

    Added the Improved also

--- geekforgeek/arrays/FirstNonRepeatingChar.java
+++ geekforgeek/arrays/FirstNonRepeatingChar.java
@@ -1,26 +1,36 @@
 package geekforgeek.arrays;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 public class FirstNonRepeatingChar {
 
-	// Over all Complexity O(2*n) => O(n)
+	// Over all Complexity is O(2*n) => O(n)   for Algo 1
+	// Over all Complexity is O(n)  for Algo 2 
 	public static void main(String[] args) {
-		final String input = "GeeksForGeeks";
+		final String input = "Geeks5Geeks";
 		Map<String, Integer> count = getCountOfAlphabets(input);
-
-		for (Entry<String, Integer> e : count.entrySet()) {
-			System.out.println("> key : " + e.getKey());
-			System.out.println("> value : " + e.getValue());
-		}
 		
 		String c = getFirstNonRepeating(count, input);
 		if(c == null)
 			System.out.println("All char are repeating");
 		else 
 			System.out.println("First Non repeating char is --> " + c.charAt(0));
+		
+		
+		System.out.println("*** Using algo 2");
+		
+		Character letter = findFirstNonRepeatingChar(input);
+		if(letter == null)
+			System.out.println("All char are repeating");
+		else 
+			System.out.println("First Non repeating char is --> " + letter);
+		
 	}
 
 	// Complexity O(n)
@@ -46,4 +56,30 @@ public class FirstNonRepeatingChar {
 		}
 		return null;
 	}
+	
+	// *************************************************************************************
+	// Best algorithm: It finds first non repeating char in a single pass hence O(n)
+	// But uses additional memory
+	// *************************************************************************************
+	public static Character findFirstNonRepeatingChar(final String input) {
+		Set<Character> repeating = new HashSet<Character>();
+		List<Character> nonrepeating = new ArrayList<Character>();
+		
+		for(int i = 0 ;i < input.length(); i++) {
+			char letter = input.charAt(i);
+			if(repeating.contains(letter))
+				continue;
+			else {
+				if(nonrepeating.contains(letter)) {
+					nonrepeating.remove((Character)letter);
+					repeating.add(letter);
+				} else {
+					nonrepeating.add(letter);
+				}
+			}
+		}
+		
+		return (nonrepeating.isEmpty()) ? null : nonrepeating.get(0);
+	} 
+	
 }
